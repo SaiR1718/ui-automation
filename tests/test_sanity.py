e19f505e9061444a5e6f67216fd8ee91c7c074d7
sai <ssainath922@gmail.com>
2026-01-11 18:38:42 +0530

    add github actions

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,8 +1,20 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.mark.sanity
 def test_home_page_load():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     driver = webdriver.Chrome()
     driver.get("https://www.google.com")
     assert "Google" in driver.title
